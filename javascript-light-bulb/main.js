adf60f33e2d480035a2d1fe76f756d3387d7c4a6
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2022-12-21 19:15:37 +0000

    Revised boolean on javascript-light-bulb.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,15 @@
-var lightSwitch = 0;
+var lightSwitch = false;
 var $roomOn = document.querySelector('.lightbulb');
 var bodySelector = document.querySelector('body');
 function changedLights() {
-  if (lightSwitch === 0) {
+  if (lightSwitch === false) {
     $roomOn.className = 'lightbulb on';
     bodySelector.style.backgroundColor = '#fef9f1';
-    lightSwitch = 1;
+    lightSwitch = true;
   } else {
     $roomOn.className = 'lightbulb off';
     bodySelector.style.backgroundColor = '#111111';
-    lightSwitch = 0;
+    lightSwitch = false;
   }
 }
 $roomOn.addEventListener('click', changedLights);

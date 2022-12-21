b0228b7149dadeea070abcfea0b8d3ae06b30676
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2022-12-21 19:54:57 +0000

    In-progress.

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,16 @@
 /* exported lastChars */
+// define the new function "firstChars" with 2 parameters (length and string)
+function lastChars(length, string) {
+  // set a variable to an empty return string
+  var returnString = '';
+  // make sure length is within the length of string
+  if (length > string.length) {
+    // set length variable to length of string to append every character
+    length = string.length;
+  }
+  // set a for loop until its length is met and incrment per loop
+  for (var i = length - 1; i > 0; i--) {
+  // append the argument string at index i
+    returnString += string[i];
+  } return returnString;
+}
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,17 @@
 /* exported numVowels */
+// define the new function "numVowels" with 1 parameter (string)
+function numVowels(string) {
+  // assign a return counter incrementing per vowel found
+  var vowelCounter = 0;
+  // make a variable to lowercase the string argument
+  var lowerCased = string.toLowerCase();
+  // go through entirety of string taking variable I as an index
+  for (var i in lowerCased) {
+    // using the lowercased variable, check to see if the variable and the index is a, e, i, o, or u
+    if (lowerCased[i] === 'a' || lowerCased[i] === 'e' || lowerCased[i] === 'i' || lowerCased[i] === 'o' || lowerCased[i] === 'u') {
+      // if statement is true, increment return counter
+      vowelCounter++;
+    } // else pass through
+  // return the return counter.
+  } return vowelCounter;
+}
